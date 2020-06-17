0374c593d360a183017f1bba2fe07cf648d00ab7
Rolando Navarro <rolandonavarro@rolandos-mbp.home>
2020-06-17 16:07:07 -0500

    JS

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,24 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
 // ttable
 
-var tbody = d3.select('tbody');
+const tbody = d3.select('tbody');
 
 // loop through though the data
 
 function createTable(data) {
     // Use d3 to update each cell's text with data
+    tbody.html("");
+
     data.forEach(function(ufoReport) {
         //Append row for each entry in ufoReport
         var row = tbody.append("tr");
+        
         Object.entries(ufoReport).forEach(function([key, value]) {
 
         //Append a cell to the row for each value in the data
-        var cell = tbody.append("td");
+        var cell = row.append("td");
 
         //Insert data into a table
         cell.text(value);
@@ -23,35 +26,22 @@ function createTable(data) {
     });
 }
 
-//Selects the submit button id by selecting id="filter-btn" from the HTML
-var submit = d3.select("#filter-btn");
+//Selects the submit button id 
+// var submit = d3.select("#filter-btn");
 
-//FUNCTION TO FILTER DATA
-submit.on("click", function() {
-        var row = d3.select("tbody").selectAll("td");
-        row.remove();
+function handleClick() {
+    const date = d3.select('#datetime').property("value");
+    let filteredData = tableData;
 
-        //Prevent the HTML page from refreshing
-        d3.event.preventDefault();
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
 
-        //Select the input element via id="datetime" from the HTML
-        var inputElement = d3.select("#datetime");
-        console.log(inputElement);
+    createTable(filteredData);
+}
 
-        //Get the value property of the input element
-        var inputValue = inputElement.property("value");
-        console.log(inputValue);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
-        let filteredData = tableData;
-
-        //Filter the tbody for input value and console.log the filtered data
-        var filtered = filteredData.filter(tbody => tbody.datetime === inputValue);
-        console.log(filtered);
-
-        //Creates filtered table 
-        createTable(filtered);
-});
-
-//Creates initial table 
 createTable(tableData);
+
 
